567bd8c1328ae6c614afd9e06e9a06b1d6b144f7
Bruno Martins <bruno.martins217@gmail.com>
2020-10-07 02:33:42 -0300

    mudanca no arquivo js

--- README.js
+++ README.js
@@ -1,5 +1,6 @@
 //Espaço reservado para o desenvolvimento e aperfeiçoamento das minhas habilidades em desenvolvimento full-stack.
 
+//Vou ter muito sucesso na programação!
 let pessoaEstudante = "euMesmo";
 let hasbilidadesPessoaEstudante = 10;
 //Eu sou Tryber e...
