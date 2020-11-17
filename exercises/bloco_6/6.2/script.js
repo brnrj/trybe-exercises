befab8dd7b2a17789dfe88a448223fe78464f9d6
Bruno Martins <bruno.martins217@gmail.com>
2020-11-17 14:13:34 -0300

    Corrigindo erro Botao Submit

--- exercises/bloco_6/6.2/script.js
+++ exercises/bloco_6/6.2/script.js
@@ -4,7 +4,7 @@ let array = []
 for(i = 0; i < estados.length; i += 1){
     array.push(estados[i].value)
 }
-//console.log(array)
+console.log(array)
 
 let arrayEstados = ["Acre", "Alagoas", "Amapá", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"]
 
@@ -12,11 +12,10 @@ let arrayEstadosValue = ["ac", "al", "ap", "ba", "ce", "df", "es", "go", "ma", "
 
 document.getElementById('input-data').DatePickerX.init({
     format: 'dd/mm/yyyy',
-    
+
 })
 
-
-/*let submitBtn = document.getElementById('btn-submit')
+let submitBtn = document.getElementById('btn-submit')
 
 submitBtn.addEventListener('click', function(event){
     event.preventDefault()
@@ -29,5 +28,5 @@ submitBtn.addEventListener('click', function(event){
     if(!(dia > 0 && dia <= 31) && (mes > 0 && mes <=12) && (ano > 0)){
         alert('Erro na data')
     }
-})*/
+})
 
