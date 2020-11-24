70b725ee25a947c7eb8c8f4b078b9a5629b1fcff
Bruno Martins <bruno.martins217@gmail.com>
2020-11-24 15:46:43 -0300

    Conteudos Parte 2

---
+++ exercises/bloco_7/7.2/conteudos_parte_2.js
@@ -0,0 +1,19 @@
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const student = (value) => Object.assign(value);
+
+console.log(student(student1));
+console.log(student(student2));
