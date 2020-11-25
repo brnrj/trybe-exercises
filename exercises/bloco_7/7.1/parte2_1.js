a3e4403dece02af8ed1799ef76236c89e5984b40
Bruno Martins <bruno.martins217@gmail.com>
2020-11-25 13:50:31 -0300

    refatorando parte 2_1

--- exercises/bloco_7/7.1/parte2_1.js
+++ exercises/bloco_7/7.1/parte2_1.js
@@ -1,11 +1,11 @@
 const fatorial = (n) => {
   if (n === 0) {
-    return console.log(1);
+    return 1;
   }
   let resultado = n;
   while (n > 2) {
     resultado *= --n;
   }
-  return console.log(resultado);
+  return resultado;
 };
-fatorial(15);
+console.log(fatorial(1));
