e50b9abb240f629c605f63ad47a96591f5ccc2cd
Bruno Martins <bruno.martins217@gmail.com>
2020-12-02 18:33:55 -0300

    Exercicio 1

---
+++ exercises/bloco_8/8.3/exercise1.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+const arrays = [['1', '2', '3'], [true], [4, 5, 6]];
+
+function flatten() {
+  const xablau = arrays.reduce((acc, element) => acc.concat(element), []);
+  return xablau;
+}
+console.log(flatten(arrays));
+
+// assert.deepEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
